43ab0c529c11b375145bd22202f66a8d4a6993bf
hast06001 <hast06001@E-PW024NW1>
2023-09-06 13:01:33 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift_1._1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Nästa rad visar vad som ska skrivas efter det som jag skrev på denna rad
+            Console.WriteLine("Du har lärt dig skriva ut tecken som &quot; och \.");
+        }
+
+    }
+}
